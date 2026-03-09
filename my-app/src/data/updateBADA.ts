da50c1e352c5d63ddfdb05ed95be7f9abf076e0b
My EVM <yo@myevm.org>
2026-03-09 15:03:04 +0000

    .

--- my-app/src/data/updateBADA.ts
+++ my-app/src/data/updateBADA.ts
@@ -9,22 +9,22 @@ export type ServerStats = {
 }
 
 export const Servers: ServerStats[] = [
-  { id: "103", name: "Edna", sales: 1876, badaPercent: 133.1, reviews: 0, rewards: 0, promoDollars: 8 },
-  { id: "104", name: "Angelica", sales: 2231, badaPercent: 110.4, reviews: 0, rewards: 0, promoDollars: 0 },
-  { id: "114", name: "Jazmin", sales: 8199, badaPercent: 121.8, reviews: 0, rewards: 0, promoDollars: 11.8 },
-  { id: "105", name: "Damian", sales: 3964, badaPercent: 121.4, reviews: 0, rewards: 0, promoDollars: 15.9 },
-  { id: "106", name: "Kianna", sales: 2206, badaPercent: 138.7, reviews: 0, rewards: 0, promoDollars: 4.4 },
-  { id: "107", name: "Lizbeth", sales: 1093, badaPercent: 152.9, reviews: 0, rewards: 0, promoDollars: 6 },
-  { id: "108", name: "Natalie", sales: 2169, badaPercent: 123.3, reviews: 0, rewards: 0, promoDollars: 0.0 },
+  { id: "103", name: "Edna", sales: 1057, badaPercent: 119.5, reviews: 0, rewards: 0, promoDollars: 0 },
+  { id: "104", name: "Angelica", sales: 2502, badaPercent: 128.9, reviews: 0, rewards: 0, promoDollars: 3.6 },
+  { id: "114", name: "Jazmin", sales: 8180, badaPercent: 119.0, reviews: 0, rewards: 0, promoDollars: 31.5 },
+  { id: "105", name: "Damian", sales: 2332, badaPercent: 121.2, reviews: 0, rewards: 0, promoDollars: 0 },
+  { id: "106", name: "Kianna", sales: 2137, badaPercent: 148.8, reviews: 0, rewards: 0, promoDollars: 5.7 },
+  { id: "107", name: "Lizbeth", sales: 3876, badaPercent: 127.1, reviews: 0, rewards: 0, promoDollars: 29.6 },
+  { id: "108", name: "Natalie", sales: 1829, badaPercent: 133, reviews: 0, rewards: 0, promoDollars: 15.7 },
 
-  { id: "109", name: "James", sales: 13746, badaPercent: 143.8, reviews: 0, rewards: 0, promoDollars: 76 },
-  { id: "111", name: "Naylah", sales: 12121, badaPercent: 140.3, reviews: 0, rewards: 0, promoDollars: 39 },
-  { id: "112", name: "Yesenia", sales: 2327, badaPercent: 156.6, reviews: 0, rewards: 0, promoDollars: 0 },
-  { id: "102", name: "Yvonne", sales: 9075, badaPercent: 153.5, reviews: 0, rewards: 0, promoDollars: 21 },
+  { id: "109", name: "James", sales: 9169, badaPercent: 142.8, reviews: 0, rewards: 0, promoDollars: 71.6 },
+  { id: "111", name: "Naylah", sales: 11654, badaPercent: 143.8, reviews: 0, rewards: 0, promoDollars: 31.4 },
+  { id: "112", name: "Yesenia", sales: 3005, badaPercent: 191.1, reviews: 0, rewards: 0, promoDollars: 5 },
+  { id: "102", name: "Yvonne", sales: 8740, badaPercent: 151, reviews: 0, rewards: 0, promoDollars: 14 },
 
 
-  { id: "115", name: "Brandi", sales: 2228, badaPercent: 154, reviews: 0, rewards: 0, promoDollars: 34.4 },
-  { id: "116", name: "Brisa", sales: 1577, badaPercent: 129.7, reviews: 0, rewards: 0, promoDollars: 20.8 },
-  { id: "120", name: "Itzel", sales: 2878, badaPercent: 144.5, reviews: 0, rewards: 0, promoDollars: 0 },
+  { id: "115", name: "Brandi", sales: 1861, badaPercent: 126.4, reviews: 0, rewards: 0, promoDollars: 21.3 },
+  { id: "116", name: "Brisa", sales: 1232, badaPercent: 141.9, reviews: 0, rewards: 0, promoDollars: 9 },
+  { id: "120", name: "Itzel", sales: 5660, badaPercent: 135.2, reviews: 0, rewards: 0, promoDollars: 0 },
 
 ]
